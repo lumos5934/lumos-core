c5d482bb7f312d6f54783ba61438be39c319598e
lumos <lumos.wge@gmail.com>
2026-01-20 19:30:00 +0900

    fix library path

--- Runtime/Constant.cs
+++ Runtime/Constant.cs
@@ -5,7 +5,7 @@
         #region >--------------------------------------------------- PATH
         
         
-        public const string PathLumosLib = "Packages/com.lumos.library";
+        public const string PathLumosLib = "Packages/com.lumos.core";
         public const string PathGlobalTemplate = PathLumosLib + "/Editor/Templates/Global.txt";
         public const string PathTestEditorTemplate = PathLumosLib + "/Editor/Templates/TestEditor.txt";
         public const string PathUITemplate = PathLumosLib + "/Editor/Templates/UI.txt";
