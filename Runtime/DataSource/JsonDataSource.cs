e29a219c3737130d5957cb542427523a7c3aae8e
lumos <lumos.wge@gmail.com>
2026-01-26 14:30:18 +0900

    refactor json data source
    * 경로 디버깅 추가
    * 초기화 안정성 추가
    * read 비동기로 변경

--- Runtime/DataSource/JsonDataSource.cs
+++ Runtime/DataSource/JsonDataSource.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using Cysharp.Threading.Tasks;
+using Firebase.Firestore;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using TriInspector;
@@ -10,78 +11,76 @@ namespace LumosLib
     [CreateAssetMenu(fileName = "Json_DataSource", menuName = "SO/Data Source/Json")]
     public class JsonDataSource : BaseDataSource
     {
-        [InfoBox("Path : C:\\Users\\%UserName%\\AppData\\LocalLow\\%Company%\\%Project%\\FileName.json")]
         [SerializeField] private string _fileName;
-      
-        private bool _isInitialized;
-        private string _path;
+        [ShowInInspector] private string FolderPath => Application.persistentDataPath;
+
         private JsonSerializerSettings _settings;
 
-        public override async UniTask WriteAsync<T>(T data)
+        private void OnEnable()
         {
-            if (!_isInitialized) Init();
-            
-            JObject root = LoadRoot();
-
-            root[data.GetType().Name] = JToken.FromObject(data, JsonSerializer.Create(_settings));
-
-            string json = root.ToString();
-            
-            await File.WriteAllTextAsync(_path, json);
-            
-            DebugUtil.Log(_path, "PATH");
-        }
-
-        public override UniTask<T> ReadAsync<T>()
-        {
-            if (!_isInitialized) Init();
-            
-            if (!File.Exists(_path))  
-                return UniTask.FromResult<T>(default);
-            
-            JObject root = LoadRoot();
-            string key = typeof(T).Name;
-
-            if (!root.TryGetValue(key, out JToken token)) return default;
-              
-            return UniTask.FromResult(token.ToObject<T>());
-        }
-
-        private void Init()
-        {
-            string path = Application.persistentDataPath;
-
-            if (!Directory.Exists(path))
-                Directory.CreateDirectory(path);
-
-            if (!_fileName.EndsWith(".json"))
-                _fileName += ".json";
-            
-            _path = Path.Combine(path, _fileName);
-
-            if (!File.Exists(_path))
-                File.WriteAllText(_path, "{}");
-
             _settings = new JsonSerializerSettings
             {
                 Formatting = Formatting.Indented,
                 NullValueHandling = NullValueHandling.Ignore
             };
-
-            _isInitialized = true;
         }
 
-        
-        private JObject LoadRoot()
+        public override async UniTask WriteAsync<T>(T data)
         {
-            if (!File.Exists(_path)) return new JObject();
-
-            string json = File.ReadAllText(_path);
+            if (!Directory.Exists(FolderPath))
+            {
+                Directory.CreateDirectory(FolderPath);
+            }
             
-            if (string.IsNullOrEmpty(json)) return new JObject();
+            JObject root = await LoadRootAsync();
 
+            root[data.GetType().Name] = JToken.FromObject(data, JsonSerializer.Create(_settings));
+
+            string json = root.ToString();
+
+            await File.WriteAllTextAsync(GetPath(), json);
+        }
+
+        public override async UniTask<T> ReadAsync<T>()
+        {
+            if (!File.Exists(GetPath()))  
+                return await UniTask.FromResult<T>(default);
+            
+            JObject root = await LoadRootAsync();
+            
+            string key = typeof(T).FullName;
+
+            if (!root.TryGetValue(key, out JToken token)) 
+                return await UniTask.FromResult<T>(default);
+            
+            return await UniTask.FromResult(token.ToObject<T>());
+        }
+        
+        private async UniTask<JObject> LoadRootAsync()
+        {
+            var path = GetPath();
+        
+            if (!File.Exists(path))
+                return new JObject();
+        
+            string json = await File.ReadAllTextAsync(path);
+        
+            if (string.IsNullOrEmpty(json))
+                return new JObject();
+        
             return JObject.Parse(json);
         }
-        
+
+        private string GetPath()
+        {
+            var path = Path.Combine(FolderPath, _fileName);
+
+            if (!path.EndsWith(".json"))
+            {
+                path += ".json";
+            }
+
+            return path;
+        }
     }
 }
